002b9a7a342d3289de502c07d749537896d054d2
EnderBombz <61323617+EnderBombz@users.noreply.github.com>
2022-01-07 13:04:17 -0300

    Was created the architeture of the backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AccountModule } from './account/account.module';
+import { BadgeModule } from './badge/badge.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AccountModule, BadgeModule],
   controllers: [AppController],
   providers: [AppService],
 })
