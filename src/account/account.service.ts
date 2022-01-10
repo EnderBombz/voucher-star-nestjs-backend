8ca8acacb122cdf0bada54e99974cc9c4588a72f
EnderBombz <61323617+EnderBombz@users.noreply.github.com>
2022-01-10 12:32:11 -0300

    The account service structure was been build, but, need to finish the posts methods.

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -8,5 +8,41 @@ export class AccountService {
   constructor(
     @InjectModel(Account.name) private accountModel: Model<Account>,
   ) {}
-  
+
+  async findAll() {
+    return this.accountModel.find().exec();
+  }
+  async findOneById(id) {
+    return this.accountModel.findById(id).exec();
+  }
+  async findOneByName(name) {
+    return this.accountModel.find({ userName: name }).exec();
+  }
+  async findOneByResistration(reg) {
+    return this.accountModel.find({ registration: reg }).exec();
+  }
+  async postAccount(body) {
+    return {};
+  }
+  async updateById(id, body) {
+    return {};
+  }
+  async updateByName(name, body) {
+    return {};
+  }
+  async updateByRegistration(reg, body) {
+    return {};
+  }
+  async deleteById(id) {
+    this.accountModel.findByIdAndDelete(id);
+    return {};
+  }
+  async deleteByName(name) {
+    this.accountModel.deleteOne({ userName: name });
+    return {};
+  }
+  async deleteByRegistration(reg) {
+    this.accountModel.deleteOne({ registration: reg });
+    return {};
+  }
 }
