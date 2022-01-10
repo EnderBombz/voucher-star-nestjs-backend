79aa718ab5ab8de3e4206681e6ffcf5f685dab86
EnderBombz <61323617+EnderBombz@users.noreply.github.com>
2022-01-10 12:17:49 -0300

    Name of the main page was changed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Voucher Star NEST.JS - REST API - 2022';
   }
 }
